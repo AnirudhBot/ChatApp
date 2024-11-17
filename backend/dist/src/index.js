2b758b0cfde9e3f59845fe0fdada70e21c87fb6c
Anirudh Sharma <anirudh1304@gmail.com>
2024-11-18 02:52:36 +0530

    deploy config

--- backend/dist/src/index.js
+++ backend/dist/src/index.js
@@ -1,30 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
-
 import authRoutes from "./routes/auth.js";
 import messageRoutes from "./routes/message.js";
-
 import dotenv from "dotenv";
 dotenv.config();
 import { app, server } from "./socket/socket.js";
-
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
-
 app.use(cookieParser());
 app.use(express.json());
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-if (process.env.NODE_ENV !== "development") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  });
-}
-
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
